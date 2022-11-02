433c2dc403a87d99dae630f1d0943409e4655990
ryanlai <ryan19880826@gmail.com>
2022-11-02 19:48:42 +0800

    pass params in route to template

---
+++ hello.py
@@ -0,0 +1,9 @@
+from flask import Flask #載入flask
+app = Flask(__name__) #像是初始app
+
+from flask import render_template #載入flask
+@app.route('/hello/<name>') #定義路由 @稱為裝飾器 用途就是把這個網頁路徑結合下一列函式 只要輸入這個網頁路徑就會執行下面的函式 通常會將網頁路徑跟函式名稱取一樣的
+def hello(name):
+        return render_template('hello.html', **locals())
+if __name__ == '__main__':
+        app.run()
